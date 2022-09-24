7ad74347eeb54925035dfbcaabac83f709c47b2c
marcusty15 <marco.troconis15@gmail.com>
2022-09-24 11:55:19 -0400

    se agregaron funcionalidades del correo

---
+++ controllers/mailController.js
@@ -0,0 +1,80 @@
+require('dotenv').config()
+
+
+const nodemailer = require('nodemailer');
+const sendEmail = (to, nombre) => new Promise((res, rej)=>{
+    let transporter = nodemailer.createTransport({
+        host: 'mail.lissetalbarracin.com',
+        port: 465,
+        secure: true,
+        auth:{
+            user: process.env.USER,
+            pass: process.env.PASSWORD
+
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    let msg = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+</head>
+<body>
+    <div style="width: 100%; background-color: white;">
+        <div style="padding: 20px 10px 20px 10px;  ">
+            <img style="width: 600px;" src='cid:res' alt=""/>
+        </div>
+    </div>
+</body>
+</html>`
+
+    let mailOptions = {
+        from: 'encuentros@lissetalbarracin.com',
+        to,
+        subject: `Gracias por registrate ${nombre}`,
+        html: msg,
+        attachments:[{
+            filename: 'RESPUESTA-WEB.png',
+            path: './public/img/RESPUESTA-WEB.png',
+            cid: 'res'
+        }
+    ]
+        
+    }
+   
+    transporter.sendMail(mailOptions,  (err, data)=>{
+        if(err){
+            rej(err)
+        } else {
+            res(true)
+        }
+    })
+})
+
+const enviarCorreo = async (req, res)=>{
+    const {para, nombre} = req.body
+
+    try {
+        const email = await sendEmail(para, nombre)
+        return res.send({
+            enviado: true,
+            email
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            enviado: false
+        })
+    }
+}
+
+module.exports = {
+    enviarCorreo
+}
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+require('dotenv').config()
+
 const express = require('express');
 const users = require('./routes/userRoutes')
-const cors = require('cors')
-
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mailRouter = require('./Routes/mailRouter');
 
 
 const app = express()
 
-const port =  8000
+
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
 app.use('/users', users)
+app.use('/email', mailRouter)
+
+app.use(express.static(__dirname + '/public'))
+
+let port = process.env.PORT
+if(port== null || port == ''){
+    port = 3000
+}
 
 app.listen(port, ()=>{
     console.log(`servidor levantado en el puerto ${port}`)
