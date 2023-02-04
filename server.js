6bdc071b34b43cb83fd946a0347fe724e4b1a1ee
Natacha Vergara <natacha.vergara@fuzzyfish.com>
2023-02-04 15:55:39 -0300

    actualizando el archivo server

--- server.js
+++ server.js
@@ -14,19 +14,26 @@ const corsOptions = {
     origin: "*",
     credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-  app.use(cors(corsOptions));
+}
+app.use(cors(corsOptions));
 app.use(express.json())
+app.use('/', (req, res) =>
+{
+    res.send('Hola Mundo')
+})
+
 app.use('/users', users)
 app.use('/email', mailRouter)
 
 app.use(express.static(__dirname + '/public'))
 
 let port = process.env.PORT
-if(port== null || port == ''){
+if (port == null || port == '')
+{
     port = 3000
 }
 
-app.listen(port, ()=>{
+app.listen(port, () =>
+{
     console.log(`servidor levantado en el puerto ${port}`)
 })
